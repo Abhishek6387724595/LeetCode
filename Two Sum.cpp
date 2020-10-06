72dd1aa2b26e0fe336b552099ad3abc379fd7334
Abhishek Awasthi <avasthiabhishek0033@gmail.com>
2020-10-07 00:02:33 +0530

    Used Unordered Map Approach

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -1,3 +1,5 @@
+
+//****************************************************** Brute Force Approach(35% Faster) **********************************************//
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) 
@@ -17,5 +19,27 @@ public:
             
         }
         return arr;
+    }
+};
+//***************************************************** Unordered Map Approach(95% Faster) *********************************************//
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>ans;
+        unordered_map<int,int> mpp;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mpp.find(target-nums[i])!=mpp.end())
+            {
+                ans.push_back(mpp[target-nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+            mpp[nums[i]] = i;
+        }
+        return ans;
+            
+
     }
 };
